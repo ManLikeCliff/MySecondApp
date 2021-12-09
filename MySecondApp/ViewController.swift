eb862f81872acba42a3758cc7ea32eb1948600ea
Cliff <klifiam@gmail.com>
2021-12-09 12:32:19 +0100

    Initial Commit

---
+++ MySecondApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySecondApp
+//
+//  Created by Wema Bank on 09/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
